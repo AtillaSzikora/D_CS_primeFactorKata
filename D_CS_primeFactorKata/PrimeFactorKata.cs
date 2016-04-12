ce1143c1e3dac918516ec078f632bbf1beee4aac
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-12 10:24:33 +0200

    Initial commit.

---
+++ D_CS_primeFactorKata/PrimeFactorKata.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactorKata {
+	public class PrimeFactors {
+
+		public static List<int> PrimeFactorsGenerate(int number) {
+			List<int> result = new List<int>();
+			int divisor = 2;
+			while (number > 1) {
+				if (number % divisor == 0) {
+					result.Add(divisor);
+					number = number / divisor; }
+				else { divisor++; } }
+			return result; }
+
+		private static void Main(string[] args) {}
+	}
+}
---
+++ PrimeFactorTests/PrimeFactorTests.cs
@@ -0,0 +1,85 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeFactorTests {
+	[TestClass]
+	public class PrimeFactorTests {
+
+		[TestMethod]
+		public void Zero_ReturnsEmptyList () {
+			//Arrange
+			List<int> result;
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(0);
+			//Assert
+			CollectionAssert.AreEqual(result, new List<int>()); }
+
+		[TestMethod]
+		public void One_ReturnsEmptyList () {
+			//Arrange
+			List<int> result;
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(1);
+			//Assert
+			CollectionAssert.AreEqual(result, new List<int>()); }
+
+		[TestMethod]
+		public void Two_ReturnsListWithValueOf2 () {
+			//Arrange
+			List<int> result;
+			List<int> temp = new List<int>();
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(2);
+			//Assert
+			temp.Add(2);
+			CollectionAssert.AreEqual(result, temp); }
+
+		[TestMethod]
+		public void Three_ReturnsListWithValueOf3 () {
+			//Arrange
+			List<int> result;
+			List<int> temp = new List<int>();
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(3);
+			//Assert
+			temp.Add(3);
+			CollectionAssert.AreEqual(result, temp); }
+
+		[TestMethod]
+		public void Four_ReturnsListWithValueOf2_2 () {
+			//Arrange
+			List<int> result;
+			List<int> temp = new List<int>();
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(4);
+			//Assert
+			temp.Add(2);
+			temp.Add(2);
+			CollectionAssert.AreEqual(result, temp); }
+
+		[TestMethod]
+		public void Six_ReturnsListWithValueOf2_3 () {
+			//Arrange
+			List<int> result;
+			List<int> temp = new List<int>();
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(6);
+			//Assert
+			temp.Add(2);
+			temp.Add(3);
+			CollectionAssert.AreEqual(result, temp); }
+
+		[TestMethod]
+		public void Nine_ReturnsListWithValueOf3_3 () {
+			//Arrange
+			List<int> result;
+			List<int> temp = new List<int>();
+			//Act
+			result = PrimeFactorKata.PrimeFactors.PrimeFactorsGenerate(9);
+			//Assert
+			temp.Add(3);
+			temp.Add(3);
+			CollectionAssert.AreEqual(result, temp); }
+	}
+}
